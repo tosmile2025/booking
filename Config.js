7014d745240ccdf144a6cfd056d212ff9981c611
tosmile <wajanachai.wo.budc@cpird.in.th>
2025-09-26 17:35:00 +0700

    no message

---
+++ Config.js
@@ -0,0 +1,11 @@
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+//LIFF_ID
+    const LIFF_ID = '2008088008-48JzVeeQ'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008088008-jWGqwvvn'; // LIFF ID history
+    const LIFF_REGISTER = '2008088008-RGaXeww4'; // LIFF ID register
+
+//// test dropdown
+  const WEB_APP_DD = "https://script.google.com/macros/s/AKfycby6ch5pUa6UtNX4UJMUKrGC_WustS2aD1PrJyrQYzZYncCDzHC8pkU0RDVpB0SEcVq_VQ/exec";
+  const WEB_APP_DATA = "https://script.google.com/macros/s/AKfycbyDd0l1lexqevqLj8nT2I9Skx6iQszkQigQgdPMv8P8zUE3wTVDuR2z8iK-sa5TGxLb/exec";
