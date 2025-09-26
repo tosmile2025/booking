7fed7a1621d171ec9edc9a1b940fbdb47e98fbc6
tosmile <wajanachai.wo.budc@cpird.in.th>
2025-09-26 16:41:32 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,11 @@
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+//LIFF_ID
+    const LIFF_ID = '2008088008-48JzVeeQ'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008088008-jWGqwvvn'; // LIFF ID history
+    const LIFF_REGISTER = '2008088008-RGaXeww4'; // LIFF ID register
+
+//// test dropdown
+  const WEB_APP_DD = "https://script.google.com/macros/s/AKfycby6ch5pUa6UtNX4UJMUKrGC_WustS2aD1PrJyrQYzZYncCDzHC8pkU0RDVpB0SEcVq_VQ/exec";
+  const WEB_APP_DATA = "https://script.google.com/macros/s/AKfycbwfctQ1idZ_x9xQHbBdB0M3q-rrsWGZBruVFNeHN4vB3dijLBnLoCuN8J23yr6TqaEo/exec";
